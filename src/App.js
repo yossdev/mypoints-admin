49b7a498f3d446760bca566e58dfb596e823d034
MohammadWawan <mohammadwawan.24@gmail.com>
2021-12-19 18:00:06 +0700

    add commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg'
-import './App.css'
+import Login from "./pages/Login/login"
+import Register from "./pages/Register/register"
+import Dashboard from "./pages/Dashboard/dashboard"
+import Agents from "./pages/Agents/agents"
+import Product from "./pages/Product/product"
+import Reward from "./pages/Reward/reward"
+import History from "./pages/History/history"
+
+import { BrowserRouter,Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div>
-      <img src={logo} alt="logo" className="bg-violet-600" />
-      <h1 className="text-3xl font-bold underline">MyPoints Admin</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/login" exact element={<Login/>}></Route>
+          <Route path="/register" exact element={<Register/>}></Route>
+          <Route path="/" exact element={<Dashboard/>}></Route>
+          <Route path="/agents" exact element={<Agents/>}></Route>
+          <Route path="/product" exact element={<Product/>}></Route>
+          <Route path="/reward" exact element={<Reward/>}></Route>
+          <Route path="/history" exact element={<History/>}></Route>
+      </Routes>
+    </BrowserRouter>
+   
   )
 }
 
