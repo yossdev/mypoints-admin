8816f9d057ac703948c435f13fd8afe7d3c78d58
MohammadWawan <mohammadwawan.24@gmail.com>
2021-12-20 14:41:43 +0700

    product skeleton

--- src/components/pages/product.jsx
+++ src/components/pages/product.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Product() {
     return (
         <div>
-            <h1>ini product</h1>
+            <h1>ini product!</h1>
         </div>
     )
 }
