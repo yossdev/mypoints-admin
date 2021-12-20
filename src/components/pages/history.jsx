08eb42b6e6d3a6a52d4089cded3d8f9559bc91f2
MohammadWawan <mohammadwawan.24@gmail.com>
2021-12-20 14:41:19 +0700

    history skeleton

--- src/components/pages/history.jsx
+++ src/components/pages/history.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function History() {
     return (
         <div>
-            <h1>ini history</h1>
+            <h1>ini history!</h1>
         </div>
     )
 }
