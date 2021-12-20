f41ffc3658642bffc69309794e019fe238b73e46
MohammadWawan <mohammadwawan.24@gmail.com>
2021-12-20 14:42:19 +0700

    skeleton reward

--- src/components/pages/reward.jsx
+++ src/components/pages/reward.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Reward() {
     return (
         <div>
-            <h1>ini reward</h1>
+            <h1>ini reward!</h1>
         </div>
     )
 }
