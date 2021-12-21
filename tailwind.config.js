14bef44ea652042ee4e0aecdb27a78fa6870b3f0
MohammadWawan <mohammadwawan.24@gmail.com>
2021-12-21 15:57:43 +0700

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
-    fontFamily: {
-      sans: ['Poppins', 'sans-serif'],
+    extend: {
+      boxShadow: ['active'],
+      fontFamily: {
+        roboto: ['Roboto', 'sans serif'],
+        poppins: ['Poppins', 'sans serif'],
+      },
+      colors: {
+        purple: '#5F2788',
+        darkpurple: '#391553',
+        fusia: '#B366EB',
+        lightpurple: '#F7F4F9',
+        grey: '#EEEEEE',
+        darkgrey: '#5C5C5C',
+      },
     },
   },
   plugins: [],
