0882a7f879a210dfe3b16bd39f9952fefa767466
MohammadWawan <mohammadwawan.24@gmail.com>
2021-12-22 16:42:26 +0700

    update layouting page agents

---
+++ src/components/UI/atoms/ButtonAddAgents.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ButtonAddAgents = () => {
+  return (
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ">
+      Add Agents
+    </button>
+  )
+}
+
+export default ButtonAddAgents
--- src/components/pages/Agents.jsx
+++ src/components/pages/Agents.jsx
@@ -1,6 +1,7 @@
 import SideBar from '../UI/organisms/SideBar'
 import AdminMenu from '../UI/molecules/AdminMenu'
 import CardAgents from '../UI/organisms/CardAgents'
+import ButtonAddAgents from '../UI/atoms/ButtonAddAgents'
 
 const Agents = () => {
   document.title = 'Agents'
@@ -9,7 +10,12 @@ const Agents = () => {
     <>
       <SideBar />
       <AdminMenu />
-      <CardAgents />
+      <div class=" absolute mt-32">
+        <div class="absolute top-0 right-20 h-72 w-72 ">
+          <ButtonAddAgents />
+        </div>
+        <CardAgents />
+      </div>
     </>
   )
 }
