e2e75f8501e5e6856249e2c04234db19bcefee5b
MohammadWawan <mohammadwawan.24@gmail.com>
2022-01-04 14:45:06 +0700

    update style page Login & Register

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,70 +1,77 @@
-import pageImage from '../assets/img/first-image.png'
+import logo from '../UI/atoms/logo.svg'
 
 const Register = () => {
   document.title = 'Register'
 
   return (
-    <div>
-      <div class="w-full flex flex-wrap font-Roboto">
-        <div class="w-full md:w-1/2 flex flex-col">
-          <div class="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32">
-            <p class="text-center text-3xl text-purple">Register</p>
-            <form
-              class="flex flex-col pt-3 md:pt-8"
-              onsubmit="event.preventDefault();"
-            >
-              <div class="flex my-8 mx-4 md:mx-2 border-b-2 border-gray-700 hover:border-green-800">
-                <label class="self-center">Username:</label>
-                <input
-                  class="w-full py-3 pl-5 md:pl-20 border-0 focus:outline-none"
-                  type="email"
-                  placeholder="yourname..."
-                  autocomplete="on"
-                  required
-                />
-              </div>
-              <div class="flex my-8 mx-4 md:mx-2 border-b-2 border-gray-700 hover:border-green-800">
-                <label class="self-center">Email:</label>
-                <input
-                  class="w-full py-3 pl-5 md:pl-20 border-0 focus:outline-none"
-                  type="email"
-                  placeholder="yourname@gmail.com"
-                  autocomplete="on"
-                  required
-                />
-              </div>
-              <div class="flex my-8 mx-4 md:mx-2 border-b-2 border-gray-700 hover:border-green-800">
-                <label class="self-center">Password:</label>
-                <input
-                  class="w-full py-3 pl-2 md:pl-8 border-0 focus:outline-none"
-                  type="password"
-                  required
-                />
-              </div>
-              <input
-                type="submit"
-                value="Register"
-                class="bg-black text-white bg-purple font-bold text-lg hover:bg-gray-700 p-2 mt-8 hover:bg-darkpurple focus:ring-4 focus:ring-blue-300 font-medium rounded-lg"
-              />
-            </form>
-            <div class="text-center pt-12 pb-12">
-              <p>
-                If your already an account.{' '}
-                <a href="/login" class="underline font-semibold">
-                  Login.
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
+    <div className="h-screen bg-purple flex items-center font-Roboto">
+      <div className="mx-auto space-y-6 bg-white shadow-md border border-white rounded-lg lg:px-10 lg:pb-8">
+        <form className="space-y-6" action="#">
+          <img className="ml-8" alt="logo" src={logo} width={175} />
 
-        <div class="w-1/2 shadow-2xl">
-          <img
-            src={pageImage}
-            class="object-cover w-full h-screen hidden md:block"
-            alt="pageImage"
-          />
-        </div>
+          <h3 className="text-xl font-medium text-purple">
+            Silahkan Daftarkan Akun Anda
+          </h3>
+
+          <div>
+            <label
+              for="username"
+              className="text-sm font-medium text-purple block mb-2"
+            >
+              Your name
+            </label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+              placeholder="username"
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              for="email"
+              className="text-sm font-medium text-purple block mb-2"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+              placeholder="name@company.com"
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              for="password"
+              className="text-sm font-medium text-purple block mb-2"
+            >
+              Your password
+            </label>
+
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="••••••••"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full text-white bg-purple hover:bg-darkpurple focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          >
+            Register
+          </button>
+        </form>
       </div>
     </div>
   )
