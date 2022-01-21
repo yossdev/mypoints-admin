60f08be3465b38129e4ebf2b1e8f9bfc36800294
adzanfsw <193100062@almaata.ac.id>
2022-01-21 23:22:36 +0700

    rev form

--- src/components/UI/organisms/Rewards/AddReward.jsx
+++ src/components/UI/organisms/Rewards/AddReward.jsx
@@ -10,65 +10,83 @@ const AddReward = (props) => {
   return (
     <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-slate-700 bg-opacity-50">
       <div className="w-2/6 mx-auto flex-col items-center my-1 pt-4 px-8 bg-white shadow border border-purple rounded-lg">
+        <div className="text-center pb-4">
+          <h1 className="text-xl text-purple font-bold font-roboto">
+            Tambah Reward
+          </h1>
+        </div>
+
+        <hr />
+
         <form
           onSubmit={handleSubmitAddReward}
           method="post"
-          className="mx-auto w-full items-center justify-center mb-4 rounded"
+          className="mx-auto w-full items-center justify-center my-4 rounded"
         >
           <div>
             <label className="block mb-2 text-sm font-bold text-purple font-roboto">
-              Judul Hadiah
+              Nama Reward <span className="text-red">*</span>
             </label>
             <input
               name="title"
               onChange={onChangeAddReward}
               className="w-full h-10 px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded"
               type="text"
+              required
             />
           </div>
 
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-2/4 px-3 mb-6 md:mb-0">
               <label className="block mb-2 text-sm font-bold text-purple font-roboto">
-                Points
+                Points <span className="text-red">*</span>
               </label>
               <div className="relative">
                 <input
                   name="points"
                   onChange={onChangeAddReward}
                   className="h-10 appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  type="text"
+                  type="number"
+                  defaultValue={1}
+                  min="1"
+                  required
                 />
               </div>
             </div>
 
             <div className="w-full md:w-2/4 px-3">
               <label className="block mb-2 text-sm font-bold text-purple font-roboto">
-                Nominal
+                Nominal <span className="text-red">*</span>
               </label>
               <input
                 name="value"
                 onChange={onChangeAddReward}
                 className="h-10 appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                type="text"
+                type="number"
+                defaultValue={1}
+                min="1"
+                required
               />
             </div>
           </div>
 
           <div>
             <label className="inline mb-2 text-sm font-bold text-purple font-roboto">
-              Gambar
+              Gambar <span className="text-red">*</span>
             </label>
             <input
               onChange={handleImg}
               className="pl-2 font-light text-sm"
               type="file"
               accept=".jpg, .jpeg, .png"
+              required
             />
           </div>
 
+          <div className="mt-4 text-sm text-red italic">* Wajib diisi</div>
+
           {error ? (
-            <div className="mt-6 text-sm text-center text-red italic">
+            <div className="my-4 text-sm text-center text-red italic">
               Terjadi kesalahan, silahkan coba lagi!
             </div>
           ) : null}
--- src/components/UI/organisms/Rewards/EditReward.jsx
+++ src/components/UI/organisms/Rewards/EditReward.jsx
@@ -11,14 +11,22 @@ const EditReward = (props) => {
   return (
     <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-slate-700 bg-opacity-50">
       <div className="w-2/6 mx-auto flex-col items-center my-1 pt-4 px-8 bg-white shadow border border-purple rounded-lg">
+        <div className="text-center pb-4">
+          <h1 className="text-xl text-purple font-bold font-roboto">
+            Edit Reward
+          </h1>
+        </div>
+
+        <hr />
+
         <form
           onSubmit={handleSubmitEditReward}
           method="put"
-          className="mx-auto w-full items-center justify-center mb-4 rounded"
+          className="mx-auto w-full items-center justify-center my-4 rounded"
         >
           <div>
             <label className="block mb-2 text-sm font-bold text-purple font-roboto">
-              Judul Hadiah
+              Nama Reward <span className="text-red">*</span>
             </label>
             <input
               onChange={onChangeEditReward}
@@ -26,13 +34,14 @@ const EditReward = (props) => {
               name="title"
               className="w-full h-10 px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded"
               type="text"
+              required
             />
           </div>
 
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-2/4 px-3 mb-6 md:mb-0">
               <label className="block mb-2 text-sm font-bold text-purple font-roboto">
-                Points
+                Points <span className="text-red">*</span>
               </label>
               <div className="relative">
                 <input
@@ -40,21 +49,27 @@ const EditReward = (props) => {
                   value={reqReward.points}
                   name="points"
                   className="h-10 appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  type="text"
+                  type="number"
+                  defaultValue={1}
+                  min="1"
+                  required
                 />
               </div>
             </div>
 
             <div className="w-full md:w-2/4 px-3">
               <label className="block mb-2 text-sm font-bold text-purple font-roboto">
-                Nominal
+                Nominal <span className="text-red">*</span>
               </label>
               <input
                 onChange={onChangeEditReward}
                 value={reqReward.value}
                 name="value"
                 className="h-10 appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                type="text"
+                type="number"
+                defaultValue={1}
+                min="1"
+                required
               />
             </div>
           </div>
@@ -71,8 +86,10 @@ const EditReward = (props) => {
             />
           </div>
 
+          <div className="mt-4 text-sm text-red italic">* Wajib diisi</div>
+
           {error ? (
-            <div className="mt-6 text-sm text-center text-red italic">
+            <div className="my-4 text-sm text-center text-red italic">
               Terjadi kesalahan, silahkan coba lagi!
             </div>
           ) : null}
